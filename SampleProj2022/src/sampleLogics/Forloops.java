e2a7a4ab3145783508d23254559aaa321adbb1d7
francis <francisyecla07@gmail.com>
2022-07-05 14:50:47 +0800

    second

--- SampleProj2022/src/sampleLogics/Forloops.java
+++ SampleProj2022/src/sampleLogics/Forloops.java
@@ -7,7 +7,7 @@ public class Forloops {
 		 String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
 		    for (String i : cars) {
 		      System.out.println(i);
-		      System.out.println(i);
+		     
 		    }
 	}
 
